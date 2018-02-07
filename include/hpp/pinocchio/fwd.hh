b2745a9a74dcf6da4c0a4ad236209c73f2e17840
Joseph Mirabel <jmirabel@laas.fr>
2018-02-07 11:51:14 +0100

    Remove hpp-model related functions

--- include/hpp/pinocchio/fwd.hh
+++ include/hpp/pinocchio/fwd.hh
@@ -3,18 +3,18 @@
 // Author: NMansard, Joseph Mirabel from Florent Lamiraux
 //
 //
-// This file is part of hpp-model
-// hpp-model is free software: you can redistribute it
+// This file is part of hpp-pinocchio
+// hpp-pinocchio is free software: you can redistribute it
 // and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version
 // 3 of the License, or (at your option) any later version.
 //
-// hpp-model is distributed in the hope that it will be
+// hpp-pinocchio is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Lesser Public License for more details.  You should have
 // received a copy of the GNU Lesser General Public License along with
-// hpp-model  If not, see
+// hpp-pinocchio  If not, see
 // <http://www.gnu.org/licenses/>.
 
 #ifndef HPP_PINOCCHIO_FWD_HH
@@ -122,4 +122,4 @@ namespace hpp {
     typedef boost::shared_ptr <const LiegroupSpace> LiegroupSpaceConstPtr_t;
   } // namespace pinocchio
 } // namespace hpp
-#endif //HPP_MODEL_FWD_HH
+#endif // HPP_PINOCCHIO_FWD_HH
