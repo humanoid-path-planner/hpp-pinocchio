73659336a08d598e0326ddc450fb0a7b53555ed3
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-12-08 18:03:57 +0100

    update example-robot-data use

--- src/urdf/util.cc
+++ src/urdf/util.cc
@@ -429,6 +429,7 @@ void loadModel(const DevicePtr_t& robot, const FrameIndex& baseFrame,
                const SE3& bMr) {
   std::vector<std::string> baseDirs = ::pinocchio::rosPaths();
   baseDirs.push_back(EXAMPLE_ROBOT_DATA_MODEL_DIR);
+  baseDirs.push_back(EXAMPLE_ROBOT_DATA_MODEL_DIR "/../..");
 
   std::string urdfFileName =
       ::pinocchio::retrieveResourcePath(urdfPath, baseDirs);
