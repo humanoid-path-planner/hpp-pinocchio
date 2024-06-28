fc186800824f8aa9edb0db444ccd22f1d76b8883
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-06-28 15:33:55 +0200

    nix: package

--- flake.nix
+++ flake.nix
@@ -1,21 +1,33 @@
 {
   description = "Description for the project";
 
+  nixConfig = {
+    extra-substituters = [ "https://gepetto.cachix.org" ];
+    extra-trusted-public-keys = [ "gepetto.cachix.org-1:toswMl31VewC0jGkN6+gOelO2Yom0SOHzPwJMY2XiDY=" ];
+  };
+
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nim65s/nixpkgs/gepetto";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    hpp-environments = {
+      url = "github:humanoid-path-planner/hpp-environments/release/5.1.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+    hpp-util = {
+      url = "github:humanoid-path-planner/hpp-util/release/5.1.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
   };
 
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-
-      ];
+      imports = [ ];
       systems = [
         "x86_64-linux"
         "aarch64-linux"
@@ -24,26 +36,17 @@
       ];
       perSystem =
         {
-          config,
           self',
-          inputs',
-          pkgs,
           system,
+          pkgs,
           ...
         }:
         {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-
-          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-          packages.default = pkgs.hello;
+          packages.default = pkgs.callPackage ./. {
+            hpp-environments = inputs.hpp-environments.packages.${system}.default;
+            hpp-util = inputs.hpp-util.packages.${system}.default;
+          };
+          devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
         };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
     };
 }
