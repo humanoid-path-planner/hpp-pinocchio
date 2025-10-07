d3682aaa2fcadeca2ff972038e8dab58e31baa75
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-07 19:06:20 +0200

    include pinocchio first
    to fix build on 22.04

--- src/urdf/util.cc
+++ src/urdf/util.cc
@@ -25,6 +25,11 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 // DAMAGE.
+
+#include <pinocchio/fwd.hpp>
+
+// include pinocchio first
+
 #include <coal/BVH/BVH_model.h>
 #include <coal/mesh_loader/loader.h>
 #include <urdf_parser/urdf_parser.h>
